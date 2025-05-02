463402cee40eb15e577fa16b4e8a7cfb68e695ba
Nouval Rizky <nouvalr@gmail.com>
2025-05-02 13:10:02 +0800

    chore: setup tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, RouterProvider } from "react-router";
 import { routes } from "./routes/index.jsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
